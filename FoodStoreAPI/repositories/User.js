53130be31c5299b37f0d06907c88b8ff56ea9c17
Pasha3amok <3amok1985@gmail.com>
2024-06-05 05:54:27 +0300

    first commit

--- FoodStoreAPI/repositories/User.js
+++
@@ -1,9 +0,0 @@
-import pool from "../db.js";
-
-class UserRepository {
-  static async createUser({ userName, hashedPassword, role }) {}
-
-  static async getUserData(userName) {}
-}
-
-export default UserRepository;
